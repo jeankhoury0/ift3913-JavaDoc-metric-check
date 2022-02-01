ea4d0142624e019ed4e6f18468871851ceea46d9
Jean Khoury <jeankhoury0@gmail.com>
2022-01-31 19:56:49 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# JavaDoc metric check
+
+Lab based on the course 'IFT3913 QUALITÉ DE LOGICIEL ET MÉTRIQUES'
+
+## Contributor
+ - @jeankhoury0
+ - @tianpeng97
---
+++ src/main/java/com/umontreal/ift3913/h22/IMetrics.java
@@ -0,0 +1,12 @@
+package com.umontreal.ift3913.h22;
+
+public interface IMetrics {
+
+    public int classe_LOC();
+    public int paquet_LOC();
+    public int classe_CLOC();
+    public int paquet_CLOC();
+    public double classe_DC();
+    public double paquet_DC();
+
+}
